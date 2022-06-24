e59aa680cbfdc29dd7dc8186a68914b3761ced3b
George Black <contact@george.black>
2022-06-23 22:39:04 -0500

    Enable PUT action on bucket

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 export default {
-  async fetch(request) {
-    return new Response("Hello World!");
+  async fetch(request, env) {
+    if (request.method !== "PUT") {
+      return new Response("Method not allowed", { status: 405 });
+    }
+    if (!authorized(request, env)) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    // Use URL to determine object key
+    // i.e. 'http://example.com/picture.jpg' -> 'picture.jpg'
+    const pathname = new URL(request.url).pathname;
+    const objectKey = pathname.replace(/^\/+/, "");
+
+    await env.WEB.put(objectKey, request.body);
+    return new Response("Success");
   },
 };
+
+function authorized(request, env) {
+  const token = request.headers.get("X-Auth-Token");
+  return token === ACCESS_TOKEN;
+}
