869ab2719f6ef88ae15b93d06dd3515766f4f6b8
George Black <contact@george.black>
2022-06-25 08:24:55 -0500

    Add bucket list operation

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 export default {
   async fetch(request, env) {
-    if (request.method !== "PUT") {
+    if (!["GET", "PUT"].includes(request.method)) {
       return new Response("Method not allowed", { status: 405 });
     }
     if (!authorized(request, env)) {
       return new Response("Unauthorized", { status: 401 });
     }
 
+    // List bucket objects
+    if (request.method === "GET") {
+      const objects = await env.WEB.list();
+      const keys = objects.objects.map((object) => object.key);
+      return new Response(JSON.stringify({ keys }), { status: 200 });
+    }
+
     // Use URL to determine object key
     // i.e. 'http://example.com/picture.jpg' -> 'picture.jpg'
-    const pathname = new URL(request.url).pathname;
-    const objectKey = pathname.replace(/^\/+/, "");
-
-    await env.WEB.put(objectKey, request.body);
-    return new Response("Success");
+    if (request.method === "PUT") {
+      const pathname = new URL(request.url).pathname;
+      const objectKey = pathname.replace(/^\/+/, "");
+      await env.WEB.put(objectKey, request.body);
+      return new Response("Success");
+    }
   },
 };
 
